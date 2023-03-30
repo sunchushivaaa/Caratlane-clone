958a3ab9a5814cfd6c70c67e41baa02b00ba49c5
Md Anwar Hossain201~ <anwarjitme.com>
2023-03-30 22:47:42 +0530

    added day2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,9 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Test from "./components/test"
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-   
-      {/* <h1>NEM-111 Construct Week</h1> */}
-      {/* <Test/> */}
-
+      <h1>NEM-111 Construct Week</h1>
     </div>
   );
 }
