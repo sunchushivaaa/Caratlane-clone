7f1f0efcf059afc93fff174af07f6db466352234
Pushkaraj Lolusare <pushkarajlolusare24@gmail.com>
2023-03-31 11:35:29 +0530

    day3

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,9 +1,10 @@
 var jwt = require('jsonwebtoken');
+require("dotenv").config()
 
 const auth = (req ,res , next)=>{
   const token = req.headers.authorization
   if(token){
-    const decoded = jwt.verify(token , "karatlogin")
+    const decoded = jwt.verify(token , process.env.key)
     if(decoded){
       
         const userID = decoded.userID
